ad2f071381d15509e81d5ab9feab4c2d55b293dc
mdrzewek <m.m.drzewek@gmail.com>
2022-09-23 19:00:04 +0200

    project setup

---
+++ src/main/java/com/drzewek/wfrp_npc_generator/WfrpNpcGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.drzewek.wfrp_npc_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfrpNpcGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfrpNpcGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drzewek/wfrp_npc_generator/WfrpNpcGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drzewek.wfrp_npc_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfrpNpcGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
