f8a63e368a22beb71a0cb513f0a0ec68d1ca7a67
mdrzewek <m.m.drzewek@gmail.com>
2022-09-29 18:07:26 +0200

    Improved test

--- src/test/java/com/drzewek/wfrp_npc_generator/controller/MainControllerTest.java
+++ src/test/java/com/drzewek/wfrp_npc_generator/controller/MainControllerTest.java
@@ -2,32 +2,27 @@ package com.drzewek.wfrp_npc_generator.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest
 class MainControllerTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private MainController mainController = new MainController();
+    private MainController mainController;
 
     @Test
-    public void shouldPrintMessage() throws Exception{
+    void shouldPrintMessage() {
         //given
         String expectedString = "Random NPC generator for WFRP II ed";
 
         //when
-
         //then
-        assertThat(this.mainController.index().getStatusCode().is2xxSuccessful());
-        assertThat(this.mainController.index().getBody().equals(expectedString));
-
-
+        assertThat(mainController.index().getStatusCode().is2xxSuccessful());
+        assertThat(mainController.index().getBody().equals(expectedString));
     }
 }
