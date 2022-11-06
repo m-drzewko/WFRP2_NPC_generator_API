646ed8c26fe0ec91a301603e522a7242a7036efd
mdrzewek <m.m.drzewek@gmail.com>
2022-11-06 14:02:27 +0100

    Added API description for getAllRaces()
    Added swagger annotations to getAllRaces() to generate documentation for it in Swagger web UI

--- src/main/java/com/drzewek/wfrp_npc_generator/controller/RaceController.java
+++ src/main/java/com/drzewek/wfrp_npc_generator/controller/RaceController.java
@@ -3,6 +3,10 @@ package com.drzewek.wfrp_npc_generator.controller;
 import com.drzewek.wfrp_npc_generator.model.Race;
 import com.drzewek.wfrp_npc_generator.model.RaceWriteDto;
 import com.drzewek.wfrp_npc_generator.service.RaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +21,11 @@ public class RaceController {
         this.service = service;
     }
 
+    @Operation(summary = "Returns all races")
+    @ApiResponse(responseCode = "200",
+        description = "Returned all races",
+        content = { @Content(mediaType = "application/json",
+            schema = @Schema(implementation = List.class)) })
     @GetMapping("/races")
     public List<Race> getAllRaces() {
         return service.getAllRaces();
