eaaca6dc505b2139c8ad51ff7d5ed53ad6e00841
mdrzewek <m.m.drzewek@gmail.com>
2022-12-20 21:55:07 +0100

    added logging to RaceService
    added logging to RaceService methods

--- src/main/java/com/drzewek/wfrp_npc_generator/service/RaceService.java
+++ src/main/java/com/drzewek/wfrp_npc_generator/service/RaceService.java
@@ -4,12 +4,16 @@ import com.drzewek.wfrp_npc_generator.mapper.RaceStatsDtoMapper;
 import com.drzewek.wfrp_npc_generator.model.Race;
 import com.drzewek.wfrp_npc_generator.model.RaceWriteDto;
 import com.drzewek.wfrp_npc_generator.repository.RaceRepository;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.jboss.logging.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RaceService {
+
+    Logger logger = LoggerFactory.logger(RaceService.class);
 
     private final RaceRepository raceRepository;
 
@@ -21,10 +25,12 @@ public class RaceService {
     }
 
     public List<Race> getAllRaces() {
+        logger.trace("Returning all races");
         return raceRepository.findAll();
     }
 
     public Race saveNewRace(RaceWriteDto raceToSave) {
+        logger.trace("Saving new race: " + raceToSave.getName());
         return raceRepository.save(mapper.dtoToRace(raceToSave));
     }
 }
