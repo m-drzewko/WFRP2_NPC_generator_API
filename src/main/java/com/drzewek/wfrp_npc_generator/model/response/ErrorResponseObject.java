b7b4329137456998307b2ac686db51eb7ecf04fc
mdrzewek <m.m.drzewek@gmail.com>
2023-01-20 21:09:18 +0100

    added responseObject and errorResponseObject

---
+++ src/main/java/com/drzewek/wfrp_npc_generator/model/response/ErrorResponseObject.java
@@ -0,0 +1,18 @@
+package com.drzewek.wfrp_npc_generator.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponseObject {
+
+    private HttpStatus statusCode;
+
+    private String message;
+}
