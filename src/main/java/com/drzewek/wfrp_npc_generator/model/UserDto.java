9abde7817f460ecc986f662c026b6ac5f76a80d8
mdrzewek <m.m.drzewek@gmail.com>
2023-04-12 21:40:58 +0200

    added user to user dto mapper

--- src/main/java/com/drzewek/wfrp_npc_generator/model/UserDto.java
+++ src/main/java/com/drzewek/wfrp_npc_generator/model/UserDto.java
@@ -1,7 +1,8 @@
 package com.drzewek.wfrp_npc_generator.model;
 
 import lombok.*;
-import java.util.List;
+
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,5 +13,5 @@ public class UserDto {
 
     private String username;
     private String email;
-    private List<NpcDto> savedNpcs;
+    private Set<NpcDto> savedNpcs;
 }
